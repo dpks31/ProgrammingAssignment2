b2d621f3d9f42f4ba3fdd9acf83880650feabf80
dpks31 <dpks31@gmail.com>
2015-10-26 04:30:12 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inverse <- NULL
+set <- function(Y){
+	x <<- y
+	inverse <<- NULL
+	}
+get <- function() x
+setinverse <- function(Inverse) inverse <<- Inverse
+getinverse <- function() inverse
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +20,41 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+if(require("corpcor")){
+	print("corpcor is loaded correctly")
+	} else {
+		print("trying to install corpcor")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("corpcor installed and loaded")
+			} else {
+			stop("could not install corpcor")
+			}
+		}
+inverse <- x$getinverse()
+if(!is.null(inverse)){
+	message("matrix is in memory")
+	return(inverse)
+	}
+message("inverse is not in memory so the inverse (if exist) is gonna be computed")
+data <- x$get()
+inverse <- pseudoinverse(data, ...)
+x$setinverse(inverse)
+inverse
+
 }
+
+
+
+#Sample to try the working
+#square matrix
+Y <- matrix(rpois(25,3), nrow = 5)
+cY <- makeCacheMatrix(Y)
+cY$get()
+cacheSolve(cY)
+cacheSolve(cY)
+invY <- cacheSolve(cY)
+invY
+
+
